fda28832be24f7475e007a8234173b9824710203
Kent Phan <kentphan2000@gmail.com>
2022-10-19 01:45:49 -0700

    add post id page

---
+++ client/pages/post/[id].tsx
@@ -0,0 +1,17 @@
+import gqlClient from "@gqlSDK/clients/gqlClient";
+import { getSdk } from "@gqlSDK/graphql/sdk";
+import { Post } from "components/Post/Post";
+import { GetServerSideProps } from "next";
+import React from "react";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+	const { Posts } = getSdk(gqlClient);
+	const posts = (await Posts({ limit: 10 })).posts;
+	return { props: { posts } };
+};
+
+const FullPost = () => {
+	return <Post id={""} username={""} avatarImage={undefined} body={""} likes={0} comments={[]} />;
+};
+
+export default FullPost;
