6770d9692baeac063291d2047633a8a17a777ecd
Kent Phan <kentphan2000@gmail.com>
2022-10-19 01:42:11 -0700

    center home page feed

--- client/pages/home.tsx
+++ client/pages/home.tsx
@@ -28,6 +28,7 @@ import useUserStore from "../stores/user";
 import { GetServerSideProps, NextPage } from "next";
 import { getSdk } from "../src/graphql/sdk";
 import { PostForm } from "../components/PostForm/PostForm";
+import Link from "next/link";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const { Posts } = getSdk(gqlClient);
@@ -44,7 +45,9 @@ interface PageProps {
 export const HomePage = ({ pageProps: { posts } }: PageProps) => {
 	const { data: session } = useSession();
 	// we have the user id from the session, we can use that to fetch the user data and store it in the user store
-	const { data: { user } = {} } = useUserQuery<UserQuery>(gqlClient, { userId: session?.user.id! });
+	const { data: { user } = {} } = useUserQuery<UserQuery>(gqlClient, {
+		userId: session?.user.id!,
+	});
 	const { setId, setName, setEmail, setAvatar, avatar } = useUserStore();
 
 	useEffect(() => {
@@ -57,21 +60,23 @@ export const HomePage = ({ pageProps: { posts } }: PageProps) => {
 	return (
 		<>
 			<NavBar marginBottom={20} />
-			<Container size="xl">
-				<Grid grow>
+			<Container size={1400}>
+				<Grid columns={18} grow>
 					<MediaQuery query="(max-width: 900px)" styles={{ display: "none" }}>
-						<Col span={2} />
+						<Col span={3}></Col>
 					</MediaQuery>
-					<Col span={7} className="flex justify-center">
+					<Col span={10} className="flex justify-center">
 						<Stack className="w-full" spacing={10}>
 							<PostForm posts={posts} />
 							{posts.map((post, index) => (
 								<Post
 									key={index}
+									id={post._id}
 									avatarImage={post.author.avatarImage}
 									username={post.author.name} // ! CHANGE THIS TO USERNAME LATER
 									body={post.body}
 									likes={post.likes}
+									comments={post.comments}
 								/>
 							))}
 						</Stack>
@@ -79,7 +84,7 @@ export const HomePage = ({ pageProps: { posts } }: PageProps) => {
 					<MediaQuery query="(max-width: 900px)" styles={{ display: "none" }}>
 						<Col span={3}>
 							<Stack spacing={10}>
-								<div className="h-96 rounded-md bg-bgPost"></div>
+								<div className="h-96 rounded-md bg-bgPost tw-border-solid  border-rose-500"></div>
 							</Stack>
 						</Col>
 					</MediaQuery>
