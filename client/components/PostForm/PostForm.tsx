b28e1183261222122f17f89169a65d8df72ba187
Kent Phan <kentphan2000@gmail.com>
2022-10-19 01:40:31 -0700

    prevent posting empty posts

--- client/components/PostForm/PostForm.tsx
+++ client/components/PostForm/PostForm.tsx
@@ -7,18 +7,20 @@ import {
 	useCreatePostMutation,
 	PostsQuery,
 } from "../../src/generated/generates";
-import gqlClient from "../../src/clients/gqlClient";
+import gqlClient from "@gqlSDK/clients/gqlClient";
 import { useRouter } from "next/router";
 
+// 	TODO: disable post button if there is no text in the textarea
 export const PostForm = ({ posts }: { posts: PostsQuery["posts"] }) => {
 	const router = useRouter();
 	const [value, setValue] = useState("");
+	const [error, setError] = useState("");
 	const { id: userId, name, avatar } = useUserStore();
 	const { mutate: createPost } = useCreatePostMutation<CreatePostMutation, Error>(gqlClient, {});
 
 	const handlePost = () => {
 		// use event target value over refs
-		//* ^only use refs when you dont want a rerender / or extract to component
+		//* ^only use refs when you dont want a rerender or extract to component
 		const newPostInput: CreatePostInput = {
 			author: {
 				_id: userId,
@@ -28,11 +30,18 @@ export const PostForm = ({ posts }: { posts: PostsQuery["posts"] }) => {
 			body: value,
 			likes: 0,
 		};
+		// dont allow empty posts
+		if (newPostInput.body.length === 0) {
+			setError("Post cannot be empty");
+			return;
+		}
+
 		// when we create post it does return a post we can use to update the state
 		// ^ THIS WILL BE SLOWER SINCE WE HAVE TO WAIT FOR THE MUTATION TO FINISH
 		createPost({ postInput: newPostInput });
+		setError(""); // reset error state
+		setValue(""); // set value to empty string after posting
 		router.replace(router.asPath);
-		// posts?.push(newPostInput); // to render client side for user experience - Optimistic Updates
 	};
 
 	return (
@@ -42,11 +51,19 @@ export const PostForm = ({ posts }: { posts: PostsQuery["posts"] }) => {
 					placeholder="Your Post"
 					size="lg"
 					minRows={4}
+					error={error}
 					value={value}
 					onChange={(event) => setValue(event.currentTarget.value)}
 				/>
 				<div className="flex justify-end">
-					<Button onClick={handlePost} color="orange" radius={5} size="sm" className="shadow-lg">
+					<Button
+						onClick={handlePost}
+						disabled={value.length === 0}
+						color="orange"
+						radius={5}
+						size="sm"
+						className="shadow-lg"
+					>
 						Post
 					</Button>
 				</div>
