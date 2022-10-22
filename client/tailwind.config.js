8932257c974a16a192c32a9b9b0ad71cec52bb46
Kent Phan <kentphan2000@gmail.com>
2022-10-22 06:27:26 -0700

    Add max-width media queries for tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -38,6 +38,22 @@ module.exports = {
 				"3/4": "75%",
 			},
 		},
+		screens: {
+			"2xl": { max: "1535px" },
+			// => @media (max-width: 1535px) { ... }
+
+			xl: { max: "1279px" },
+			// => @media (max-width: 1279px) { ... }
+
+			"max-lg": { max: "1000px" },
+			// => @media (max-width: 1023px) { ... }
+
+			md: { max: "767px" },
+			// => @media (max-width: 767px) { ... }
+
+			sm: { max: "639px" },
+			// => @media (max-width: 639px) { ... }
+		},
 	},
 	plugins: [
 		function ({ matchUtilities, theme }) {
